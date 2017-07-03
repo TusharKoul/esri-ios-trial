e46db6e5206208205ba1e46fdd765f0aaf978d46
Tushar Koul <tusharkoul@gmail.com>
2017-07-03 15:07:06 -0700

    cleaning up custom view

--- esriTrial1/CustomCalloutView.swift
+++ esriTrial1/CustomCalloutView.swift
@@ -13,9 +13,20 @@ class CustomCalloutView : UIView {
     
     @IBOutlet weak var placeLabel: UILabel!
     @IBOutlet weak var placeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var onSave: ((_ selectedIndex:Int) -> Void)?
+
     
     class func instanceFromNib() -> UIView {
         return UINib(nibName: "CustomCalloutView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! UIView
     }
 
+    @IBAction func segmentSelectionChanged(_ sender: Any) {
+        self.saveButton.isEnabled =  (placeSegmentedControl.selectedSegmentIndex < 0) ? false : true
+    }
+    
+    @IBAction func saveClicked(_ sender: Any) {
+        onSave?(placeSegmentedControl.selectedSegmentIndex)
+    }
 }
